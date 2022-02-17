478f6792b86326dcd3bd224d54ba52c60c2d3015
Sean Chen <qianlonwork@outlook.com>
2022-02-17 20:37:44 +0800

    2022-02-17 20:37

---
+++ Git_Auto_Commit.js
@@ -0,0 +1,76 @@
+const { exec } = require('child_process');
+
+// For todays date;
+Date.prototype.today = function () {
+  return (
+    this.getFullYear() +
+    '-' +
+    (this.getMonth() + 1 < 10 ? '0' : '') +
+    (this.getMonth() + 1) +
+    '-' +
+    (this.getDate() < 10 ? '0' : '') +
+    this.getDate()
+  );
+};
+
+// For the time now
+Date.prototype.timeNow = function () {
+  return (
+    (this.getHours() < 10 ? '0' : '') +
+    this.getHours() +
+    ':' +
+    (this.getMinutes() < 10 ? '0' : '') +
+    this.getMinutes()
+  );
+};
+var newDate = new Date();
+
+const datetime = `${newDate.today()} ${newDate.timeNow()}`;
+
+function add() {
+  exec(`git add .`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    commit();
+    console.log(`stdout: ${stdout}`);
+  });
+}
+
+function commit() {
+  exec(`git commit -m "${datetime}" `, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    if (stdout) {
+      push();
+      console.log(`stdout: ${stdout}`);
+    }
+  });
+}
+
+function push() {
+  exec(`git push -u origin master`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
+
+add();
